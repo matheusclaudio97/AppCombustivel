7a9b6b8d2f79933e65151ab72a1e670b18144a04
matheusclaudio97 <matheusclaudios@hotmail.com>
2020-11-27 15:31:32 -0300

    First APP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+
+  public precoAlcool: string = " ";
+  public precoGasolina: string= " ";
+
+  public resultado: string = "Resultado";
+ 
+
+  calcular(){
+    if(this.precoAlcool && this.precoGasolina){
+        var pAlcool = parseFloat(this.precoAlcool);
+        var pGasolina = parseFloat(this.precoGasolina);
+
+
+        var res = pAlcool / pGasolina
+        if(res >= 0.7){
+          this.resultado = "Melhor utilizar a Gasolina!";
+        
+        }else{
+          this.resultado = "Melhor utilizar Álcool";
+        }
+
+
+
+    }else{
+      this.resultado = "Preencha corretamente os campos!";
+
+
+    }
+  }
+
   constructor() {}
 
 }
